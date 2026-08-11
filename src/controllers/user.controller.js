96b2994e20edaba4748b2e32427603edf22cbb18
suraj kiran shewale <surajkiranshewale@Mac.mshome.net>
2026-08-11 20:22:07 +0530

    Day 5 Filtering added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,11 +10,52 @@ const getProfile =  (req, res) => {
 
 const getAllUsers = async (req,res) => {
     try {
-        const users = await User.find();
+
+        const page =  Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        const role = req.query.role;
+        const search = req.query.search;
+        const sortField = req.query.sort || 'createdAt'; 
+
+        const filter = {};
+
+        if(role){
+            filter.role = role;
+        }
+
+        if(search){
+            filter.$or = [
+                { name : { $regex : search , $options : 'i'}},
+                { email : { $regex : search , $options : 'i'}}
+            ]
+        }
+
+        const sortOrder =
+            sortField.startsWith("-")
+                ? -1
+                : 1;
+
+
+        const field =
+            sortField.startsWith("-")
+                ? sortField.substring(1)
+                : sortField;
+
+
+        const users = await User.find(filter)
+            .sort({
+                [field]: sortOrder
+            })
+            .skip(skip)
+            .limit(limit);
+
 
         res.status(200).json({
             success : true,
             message : 'Users retrieved successfully',
+            page,
+            limit,
             data : users 
         })
     } catch (err) {
